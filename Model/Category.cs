ba2f7a9c064f11978a037a3bb9961a43c58d5e70
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-05-21 13:22:16 +0200

    Category int changed to uint

--- Model/Category.cs
+++ Model/Category.cs
@@ -2,12 +2,12 @@
 {
     public class Category
     {
-        public int CategoryId { get; private set; }
-        public int MenuId { get; private set; }
+        public uint CategoryId { get; private set; }
+        public uint MenuId { get; private set; }
         public string CategoryType { get; private set; }
         public bool Alcoholic { get; private set; }
 
-        public Category(int categoryId, int menuId, string categoryType, bool alcoholic)
+        public Category(uint categoryId, uint menuId, string categoryType, bool alcoholic)
         {
             CategoryId = categoryId;
             MenuId = menuId;
