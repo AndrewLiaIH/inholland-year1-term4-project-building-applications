c0ac2c293594aa7a003d1b3bdf594f01ce9f94ed
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-05-21 13:09:43 +0200

    MenuCard added

---
+++ Model/MenuCard.cs
@@ -0,0 +1,14 @@
+﻿namespace Model
+{
+    public class MenuCard
+    {
+        public int CardId { get; private set; }
+        public string MenuType { get; private set; }
+
+        public MenuCard(int cardId, string menuType)
+        {
+            CardId = cardId;
+            MenuType = menuType;
+        }
+    }
+}
