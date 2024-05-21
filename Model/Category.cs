cb3cda94eec8661b00aec47d80275e53ac0b20e6
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-05-21 13:13:25 +0200

    Category added

---
+++ Model/Category.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public class Category
+    {
+        public int CategoryId { get; private set; }
+        public int MenuId { get; private set; }
+        public string CategoryType { get; private set; }
+        public bool Alcoholic { get; private set; }
+
+        public Category(int categoryId, int menuId, string categoryType, bool alcoholic)
+        {
+            CategoryId = categoryId;
+            MenuId = menuId;
+            CategoryType = categoryType;
+            Alcoholic = alcoholic;
+        }
+    }
+}
