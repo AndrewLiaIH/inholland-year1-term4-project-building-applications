510ad1a845969c797c82427540d2b0661a8052bb
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-05-22 18:29:05 +0200

    Enums added for Category and ManuCard

--- Model/Category.cs
+++ Model/Category.cs
@@ -4,10 +4,10 @@
     {
         public uint CategoryId { get; private set; }
         public MenuCard MenuCard { get; private set; }
-        public string CategoryType { get; private set; }
+        public CategoryType CategoryType { get; private set; }
         public bool Alcoholic { get; private set; }
 
-        public Category(uint categoryId, MenuCard menuCard, string categoryType, bool alcoholic)
+        public Category(uint categoryId, MenuCard menuCard, CategoryType categoryType, bool alcoholic)
         {
             CategoryId = categoryId;
             MenuCard = menuCard;
---
+++ Model/CategoryType.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public enum CategoryType
+    {
+        LunchStarters,
+        LunchMains,
+        LunchDesserts,
+        DinnerStarters,
+        DinnerEntrements,
+        DinnerMains,
+        DinnerDesserts,
+        SoftDrinks,
+        BeersOnTap,
+        Wines,
+        SpiritDrinks,
+        CoffeeTea
+    }
+}
