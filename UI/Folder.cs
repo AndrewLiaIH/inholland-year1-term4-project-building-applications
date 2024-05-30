3f36837aeaa02911b1f30aaa3d967a89db249914
Anastasia Yurashchuk <asu45456@gmail.com>
2024-05-30 19:24:05 +0200

    Updated logic for folders and the temporary ones are removed

--- UI/Folder.cs
+++ UI/Folder.cs
@@ -1,8 +1,16 @@
-﻿namespace UI
+﻿using System.Windows.Controls;
+
+namespace UI
 {
     public class Folder
     {
-        public string Name { get; set; }
-        public string Type { get; set; }
+        public string Name { get; }
+        public UserControl UserControl { get; }
+
+        public Folder(string name, UserControl userControl)
+        {
+            Name = name;
+            UserControl = userControl;
+        }
     }
 }
