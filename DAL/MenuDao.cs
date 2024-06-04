c18dfe81777cacaddad31091ee5af55a97f0554e
Anastasia Yurashchuk <asu45456@gmail.com>
2024-06-04 08:29:16 +0200

    Order and Menu united at DAL and Service layers

---
+++ DAL/MenuDao.cs
@@ -0,0 +1,121 @@
+﻿using Model;
+using System.Data;
+
+namespace DAL
+{
+    public class MenuDao : BaseDao
+    {
+        // Menu Card
+        private const string QueryGetAllMenuCards = $"SELECT {ColumnCardId}, {ColumnMenuType} FROM menu_card";
+        private const string QueryGetMenuCardById = $"{QueryGetAllMenuCards} WHERE {ColumnCardId} = {ParameterNameCardId}";
+
+        private const string ColumnCardId = "card_id";
+        private const string ColumnMenuType = "menu_type";
+
+        private const string ParameterNameCardId = "@cardId";
+
+        // Category
+        private const string QueryGetAllCategories = $"SELECT {ColumnCategoryId}, {ColumnMenuId}, {ColumnCategoryType}, {ColumnAlcoholic} FROM category";
+        private const string QueryGetCategoryById = $"{QueryGetAllCategories} WHERE {ColumnCategoryId} = {ParameterNameCategoryId}";
+
+        private const string ColumnCategoryId = "category_id";
+        private const string ColumnMenuId = "menu_id";
+        private const string ColumnCategoryType = "category_type";
+        private const string ColumnAlcoholic = "alcoholic";
+
+        private const string ParameterNameCategoryId = "@categoryId";
+
+        // Menu Item
+        private const string QueryGetAllMenuItems = $"SELECT {ColumnItemId}, {ColumnItemCategoryId}, {ColumnItemNumber}, {ColumnStockAmount}, {ColumnOnMenu}, {ColumnPrice}, {ColumnDescription}, {ColumnName}, {ColumnShortName} FROM menu_item";
+        private const string QueryGetMenuItemById = $"{QueryGetAllMenuItems} WHERE {ColumnItemId} = {ParameterNameItemId}";
+
+        private const string ColumnItemId = "item_id";
+        private const string ColumnItemCategoryId = "category_id";
+        private const string ColumnItemNumber = "item_number";
+        private const string ColumnStockAmount = "stock_amount";
+        private const string ColumnOnMenu = "on_menu";
+        private const string ColumnPrice = "price";
+        private const string ColumnDescription = "description";
+        private const string ColumnName = "name";
+        private const string ColumnShortName = "short_name";
+
+        private const string ParameterNameItemId = "@itemId";
+
+        public List<MenuCard> GetAllMenuCards()
+        {
+            return GetAll(QueryGetAllMenuCards, ReadRowMenuCard);
+        }
+
+        public MenuCard GetMenuCardById(int cardId)
+        {
+            Dictionary<string, int> parameters = new()
+            {
+                { ParameterNameCardId, cardId }
+            };
+
+            return GetByIntParameters(QueryGetMenuCardById, ReadRowMenuCard, parameters);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return GetAll(QueryGetAllCategories, ReadRowCategory);
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            Dictionary<string, int> parameters = new()
+            {
+                { ParameterNameCategoryId, categoryId }
+            };
+
+            return GetByIntParameters(QueryGetCategoryById, ReadRowCategory, parameters);
+        }
+
+        public List<MenuItem> GetAllMenuItems()
+        {
+            return GetAll(QueryGetAllMenuItems, ReadRowMenuItem);
+        }
+
+        public MenuItem GetMenuItemById(int itemId)
+        {
+            Dictionary<string, int> parameters = new()
+            {
+                { ParameterNameItemId, itemId }
+            };
+
+            return GetByIntParameters(QueryGetMenuItemById, ReadRowMenuItem, parameters);
+        }
+
+        private MenuCard ReadRowMenuCard(DataRow dr)
+        {
+            int cardId = (int)dr[ColumnCardId];
+            bool parsedMenuType = Enum.TryParse((string)dr[ColumnMenuType], out MenuType menuType);
+            return new MenuCard(cardId, menuType);
+        }
+
+        private Category ReadRowCategory(DataRow dr)
+        {
+            int categoryId = (int)dr[ColumnCategoryId];
+            MenuCard menuCard = GetMenuCardById((int)dr[ColumnMenuId]);
+            bool parsedCategoryType = Enum.TryParse((string)dr[ColumnCategoryType], true, out CategoryType categoryType);
+            bool? alcoholic = dr[ColumnAlcoholic] as bool?;
+
+            return new(categoryId, menuCard, categoryType, alcoholic);
+        }
+
+        private MenuItem ReadRowMenuItem(DataRow dr)
+        {
+            int itemId = (int)dr[ColumnItemId];
+            Category category = GetCategoryById((int)dr[ColumnCategoryId]);
+            int? itemNumber = dr[ColumnItemNumber] as int?;
+            int? stockAmount = dr[ColumnStockAmount] as int?;
+            bool onMenu = (bool)dr[ColumnOnMenu];
+            decimal? price = dr[ColumnPrice] as decimal?;
+            string? description = dr[ColumnDescription] as string;
+            string? name = dr[ColumnName] as string;
+            string? shortName = dr[ColumnShortName] as string;
+
+            return new(itemId, category, itemNumber, stockAmount, onMenu, price, description, name, shortName);
+        }
+    }
+}
---
+++ Service/MenuService.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using Model;
+
+namespace Service
+{
+    public class MenuService
+    {
+        private MenuDao menuDao = new();
+
+        public List<MenuCard> GetAllMenuCards()
+        {
+            return menuDao.GetAllMenuCards();
+        }
+
+        public MenuCard GetMenuCardById(int cardId)
+        {
+            return menuDao.GetMenuCardById(cardId);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return menuDao.GetAllCategories();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return menuDao.GetCategoryById(id);
+        }
+
+        public List<MenuItem> GetAllMenuItems()
+        {
+            return menuDao.GetAllMenuItems();
+        }
+
+        public MenuItem GetMenuItemById(int itemId)
+        {
+            return menuDao.GetMenuItemById(itemId);
+        }
+    }
+}
